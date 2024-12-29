b1e266d7daf0296acb52d0212627e548f626922b
nihadgojayev <nihadqocayev152@gmail.com>
2024-12-29 12:01:40 -0800

    first

--- src/projectEuler001.java
+++ src/projectEuler001.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class projectEuler001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0, b = 0, sum1 = 0, sum2 = 0, target = scanner.nextInt();
+        while (a + 3 < target){
+            a += 3;
+            sum1 += a;
+        }
+        while (b + 5 < target){
+            b += 5;
+            if (b % 3 == 0){
+                continue;
+            }
+            sum2 += b;
+        }
+        System.out.println(sum1 + sum2);
+    }
 }
--- src/projectEuler002.java
+++ src/projectEuler002.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class projectEuler002 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        Set<Integer> set = new HashSet<>();
+        int maxx = -1;
+
+        for (int i = 2; i <= n; i ++){
+            while(n % i == 0){
+                n /= i;
+                set.add(i);
+                maxx = Math.max(maxx, i);
+            }
+        }
+        System.out.println(set);
+        System.out.println(maxx);
+
+    }
+
+
 }
