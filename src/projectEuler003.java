0c7e40d7ebdc18c5a0f55efd06291e61b608b7eb
nihadgojayev <nihadqocayev152@gmail.com>
2024-12-31 15:07:23 -0800

    newOne

--- src/projectEuler003.java
+++ src/projectEuler003.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class projectEuler003 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        long target = 600851475143L;
+        long maxx = -1;
+        for (long i = 2; i <= target; i++) {
+            while (target % i == 0) {
+                target /= i;
+                maxx = i;
+            }
+        }
+        System.out.println(maxx);
     }
 }
