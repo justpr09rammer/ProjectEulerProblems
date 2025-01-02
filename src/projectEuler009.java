fe26725cd5610ee3122bc326fc3cf22da7890fbb
nihadgojayev <nihadqocayev152@gmail.com>
2025-01-02 08:15:36 -0800

    problem009bruteForceWithTwoLoops

---
+++ src/projectEuler009.java
@@ -0,0 +1,26 @@
+public class projectEuler009 {
+    public static void main(String[] args) {
+        //brute force approach with two loops
+
+
+
+
+        for (int c = 997; c >= 1; c --){
+            int sumOfAandB = 1000 - c;
+            for (int a = 1; a < sumOfAandB; a++){
+                int b = sumOfAandB - a;
+                if (b < a){
+                    break;
+                }
+                else {
+                    if (a * a + b * b == c * c){
+                        System.out.println(a * b * c);
+                        return;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
