bac36ccdeafa9ccb311af16cca9fb5de928eb5d4
nihadgojayev <nihadqocayev152@gmail.com>
2025-01-28 05:52:36 -0800

    problem 16

--- src/projectEuler016.java
+++ src/projectEuler016.java
@@ -1,37 +1,32 @@
 import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
 import java.math.BigInteger;
 
 public class projectEuler016 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long n = scanner.nextLong();
-        BigInteger ans1 = power(n);
-
-        long ans2 = digitSum(ans1);
-        System.out.println(ans2);
+        Long n = scanner.nextLong();
+        BigInteger number = power(n);
+        System.out.println(number);
+        System.out.println(digitSum(number));
     }
 
-    public static BigInteger power(long exponent) {
-        BigInteger result = BigInteger.ONE;
-        BigInteger base = BigInteger.valueOf(2);
-        while (exponent > 0) {
-            if (exponent % 2 != 0) {
-                result = result.multiply(base);
-            }
-            base = base.multiply(base);
-            exponent /= 2;
+    public static BigInteger power(Long n) {
+        if (n == 0) {
+            return BigInteger.ONE;
         }
-        return result;
+        if (n % 2 != 0) {
+            return BigInteger.valueOf(2).multiply(power(n - 1));
+        }
+        else {
+            BigInteger halfPower = power(n / 2);
+            return halfPower.multiply(halfPower);
+        }
     }
-    public static long digitSum(BigInteger number) {
-        long sum = 0;
-        String numStr = number.toString();
-        for (int i = 0; i < numStr.length(); i++) {
-            sum += Character.getNumericValue(numStr.charAt(i));
+    public static BigInteger digitSum(BigInteger number) {
+        String numberString = number.toString();
+        BigInteger sum = BigInteger.ZERO;
+        for (int i = 0; i < numberString.length(); i++) {
+            sum = sum.add(BigInteger.valueOf(Character.getNumericValue(numberString.charAt(i))));
         }
         return sum;
     }
