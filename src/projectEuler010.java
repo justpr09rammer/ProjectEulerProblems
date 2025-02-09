d49ed60550d0a432e9998f560c694b7bf600473e
nihadgojayev <nihadqocayev152@gmail.com>
2025-02-09 07:23:51 -0800

    Sieve of Eratosthenes Algorithm

--- src/projectEuler010.java
+++ src/projectEuler010.java
@@ -1,26 +1,26 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class projectEuler010 {
     public static void main(String[] args) {
-        Set<Long> set = new HashSet<>();
-        set.add(2L);
-        set.add(3L);
-        long sum = 2 + 3;
-        for (long i = 5; i < 2000000; i ++){
-            boolean isPrime = true;
-            for (long element : set){
-                if (i % element == 0){
-                    isPrime = false;
-                    break;
+        System.out.println(findSumOfPrimes(2000000));
+    }
+    public static long findSumOfPrimes(int n){
+        long[] primes = new long[n + 1];
+        long sumOfPrimes = 0;
+        for (int i = 2; i <= n; i++) {
+            primes[i] = 1;
+        }
+        for (long i = 2; i <= n; i ++){
+            if (primes[(int) i] == 1){
+                sumOfPrimes += i;
+                for (long j = (long) i * i; j <= n; j += i) {
+                    primes[(int) j] = 0;
                 }
             }
-            if (isPrime){
-                set.add(i);
-                sum += i;
-                System.out.println(i);
-            }
         }
-        System.out.println(sum);
+        return sumOfPrimes;
     }
 }
